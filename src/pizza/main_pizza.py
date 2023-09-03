efbc69ca0c1b9c10f898584ee5567f598ce1ae49
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-09-04 01:06:35 +0300

    Fix function with latency

--- src/pizza/main_pizza.py
+++ src/pizza/main_pizza.py
@@ -1,5 +1,8 @@
 import random
+import sys
 import time
+from collections import UserDict
+from typing import Self
 
 import click
 
@@ -16,20 +19,31 @@ class PizzaRecipe:
 
     def __init__(self, size='L'):
         self.size = size
+        self.name = self.__class__.__name__
 
-    def __eq__(self, other: __class__):
-        return set(self.recipe) == set(other.recipe)
+    @classmethod
+    @property
+    def clean_recipe(cls):
+        return ', '.join([i for i in cls.recipe])
+
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
+    def __eq__(self, other: Self) -> bool:
+        return (set(self.recipe) == set(other.recipe)) & (self.name == other.name)
 
     def dict(self):
         print(self.recipe)
 
 
-assortment = {}
-
-
 def add_latency(fn):
-    time.sleep(random.randint(1, 3))
-    return fn
+    def wrapper(*args, **kwargs):
+        time.sleep(random.randint(1, 3) / 5)
+        result = fn(*args, **kwargs)
+        return result
+
+    # time.sleep(0)
+    return wrapper
 
 
 def log(str_template: str):
@@ -38,8 +52,7 @@ def log(str_template: str):
             time_start = time.time()
             # @wraps(fn) # apply wrapper from functools
             result = fn(*args, **kwargs)
-            time_end = time.time()
-            lapsed_time = time_end - time_start
+            lapsed_time = time.time() - time_start
             print(str_template.format(lapsed_time))  # apply to placeholder
             return result
 
@@ -48,8 +61,27 @@ def log(str_template: str):
     return outer_wrapper
 
 
+class CustomDict(UserDict):
+    def __init__(self):
+        super().__init__()
+
+    def __getitem__(self, item: str):
+        """To accept pizza name indifferent of register"""
+        return super().__getitem__(item.lower())
+
+    def __setitem__(self, key, value):
+        return super().__setitem__(key.lower(), value)
+
+    def __contains__(self, item: str):
+        # think how to make it work
+        return super().__contains__(item.lower())
+
+
+assortment = CustomDict()
+
+
 def pizza_to_assortment(cls):
-    assortment[cls.__name__] = cls.recipe
+    assortment[cls.__name__] = cls
     return cls
 
 
@@ -68,10 +100,14 @@ class Hawaiian(PizzaRecipe):
     recipe = ["tomato sauce", "mozzarella", "chicken", "pineapples"]
 
 
+menu_str = ""
+for k, v in assortment.items():
+    menu_str += f"- {k} 🧀: {v.clean_recipe}\n"
+
+
 @cli.command()
 def menu():
-    for k, v in assortment.items():
-        print(f"- {k} 🧀: {', '.join([i for i in v])}")
+    print(menu_str)
 
 
 @cli.command()
@@ -79,20 +115,37 @@ def menu():
 @click.argument('pizza', nargs=1)
 def order(pizza: str, delivery: bool):
     print('i want to order', pizza)
-    bake(pizza)
+    if pizza not in assortment:
+        print("No such pizza in the assortment, here's the menu:")
+        print(menu_str)
+        sys.exit()
+    ordered_pizza = assortment[pizza]()
+    bake(ordered_pizza)
     if delivery:
-        deliver(pizza)
+        deliver(ordered_pizza)
+    else:
+        pick_up(ordered_pizza)
 
 
+@log("Picking up took {:.2f} seconds")
+@add_latency
+def pick_up(pizza):
+    print('picked up')
+
+
+@log("Cooking took {:.2f} seconds")
 @add_latency
 def bake(pizza):
     print('baked')
 
 
+@log("Delivery took {:.2f} seconds")
 @add_latency
 def deliver(pizza):
-    pass
+    print('delivered')
 
 
 if __name__ == '__main__':
-    cli()
+    # cli()
+    a = Pepperoni()
+    print(a)
