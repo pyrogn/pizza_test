49d2225ec6ade7e837b2857da974e078cc005129
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-09-07 00:58:49 +0300

    Add test for incorrect pizza name

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,7 @@ def test_menu():
     """Test that there are 3 pizzas on the menu"""
     runner = CliRunner()
     result = runner.invoke(cli, ["menu"])
+    assert result.exit_code == 0
     assert len(result.output.split("\n")) == 1 + len(
         pizza_menu
     )  # one extra for header
@@ -19,11 +20,30 @@ def test_menu():
 def test_pizza_order(is_delivery, pizza_class):
     """Test that you can order pizzas and output matches target number of rows"""
     runner = CliRunner()
-    pizza_name = pizza_class.get_name()
+    pizza_name = (
+        pizza_class.get_name()
+    )  # maybe add tests for upper and lower pizza names
     params_cli = (
         ["order", pizza_name, "--delivery"]
         if is_delivery
         else ["order", pizza_name]
     )
     result = runner.invoke(cli, params_cli)
+    assert result.exit_code == 0
     assert len(result.output.split("\n")) == 4
+
+
+@all_types_delivery
+def test_pizza_incorrect_order(is_delivery):
+    """Test that you can order pizzas and output matches target number of rows"""
+    runner = CliRunner()
+    pizza_name = "some_definitely_unknown_pizza"
+    params_cli = (
+        ["order", pizza_name, "--delivery"]
+        if is_delivery
+        else ["order", pizza_name]
+    )
+    result = runner.invoke(cli, params_cli)
+    assert len(result.output.split("\n")) == 1 + 1 + len(
+        pizza_menu
+    )  # header + msg + menu
