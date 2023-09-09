d8579ec1149a3cdb5b9f96477ad00d5d46c95196
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-09-10 00:36:56 +0300

    Simplify pizza validation

--- src/pizza/pizza_menu.py
+++ src/pizza/pizza_menu.py
@@ -7,7 +7,7 @@ Classes:
             Hawaiian
     LowerKeyMenu"""
 from collections import UserDict
-from typing import Optional, TypeVar
+from typing import TypeVar
 
 
 class FoodItem:
@@ -56,41 +56,6 @@ class Pizza(FoodItem):
             string_error = f"{size} size is not in {AVAILABLE_PIZZA_SIZES}"
             raise ValueError(string_error)
         self.size = size
-
-    @classmethod
-    def safe_init(
-        cls,
-        pizza_name: str,
-        size: str = "L",
-    ) -> tuple[Optional["Pizza"], bool, str]:
-        """Safe init of Pizza subclasses
-        You will get only correct instance or no instance in case of any error
-        Attributes:
-            pizza_name - pizza name from the menu
-            size - size from the available sizes
-        Returns:
-            Pizza subclass instance or None in case of error
-            is_success (bool) - True if no error, else False
-            message - if there's an error, message=meaningful text error
-                if it is not an error, it will contain pizza name, emoji and size
-        CONFUSED: I am not sure if this is the right design to encapsulate logic
-        """
-        size = size.upper()
-        if size not in AVAILABLE_PIZZA_SIZES:
-            message = (
-                f"size {size} is not available. "
-                f"Choose one from: {AVAILABLE_PIZZA_SIZES}"
-            )
-            return None, False, message
-        if pizza_name not in pizza_menu:
-            message = (
-                f"No such pizza on the menu, the available pizzas:\n{full_menu_str}"
-            )
-            return None, False, message
-
-        cls_pizza = pizza_menu[pizza_name]
-        message = f"{cls_pizza.get_name()} {cls_pizza.emoji} {size} size"
-        return cls_pizza(size=size), True, message
 
     def bake(self) -> None:
         """Make pizza bake itself
@@ -194,6 +159,37 @@ class Hawaiian(Pizza):
     recipe = ("tomato sauce", "mozzarella", "chicken", "pineapples")
     emoji = "🍍"
     alt_name = "Hawaiian Special"  # Give a complex name for a test
+
+
+def validate_pizza(
+    pizza_name: str,
+    size: str = "L",
+) -> tuple[bool, str]:
+    """Validate if pizza with parameters exists and return log message
+    Attributes:
+        pizza_name - pizza name from the menu
+        size - size from the available sizes
+    Returns:
+        is_success (bool) - True if no error, else False
+        message - if there's an error, message is meaningful text error
+            if it is not an error, it will contain pizza name, emoji and size
+    """
+    size = size.upper()
+    if size not in AVAILABLE_PIZZA_SIZES:
+        message = (
+            f"size {size} is not available. "
+            f"Choose one from: {AVAILABLE_PIZZA_SIZES}"
+        )
+        return False, message
+    if pizza_name not in pizza_menu:
+        message = (
+            f"No such pizza on the menu, the available pizzas:\n{full_menu_str}"
+        )
+        return False, message
+
+    cls_pizza = pizza_menu[pizza_name]
+    message = f"{cls_pizza.get_name()} {cls_pizza.emoji} {size} size"
+    return True, message
 
 
 # full menu as a single multiline string
